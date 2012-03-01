b9f85c7721ed6d3f4b2b5dcc81dc8df53f8afdbf
Cargo Kult <cargokultist@gmail.com>
2012-03-01 22:09:18 +0200

    added parse json reddit call

--- site/javascript/herbarium.js
+++ site/javascript/herbarium.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    //$.getJSON('http://www.reddit.com/r/CityPorn/.json?callback=?', function(json) {
-    $.getJSON('http://www.reddit.com/user/mercer/about.json?callback=?', function(json) {
-        //$('#debug').text(json); 
-        /*$.each(data.children, function(i,item){
-            $("<img/>").attr("src", item.data.url).appendTo("#images");
-        });*/
+    $.getJSON('http://www.reddit.com/r/CityPorn/.json?jsonp=?', function(json) {
+        console.log(json);
+        $.each(json.data.children, function(i,item){
+            $('#wallpaper').delay(1500).fadeOut(500);
+	    var img = $('<img/>').attr('src', item.data.url);
+	    $('#wallpaper').empty().append(img);
+	    $('#wallpaper').fadeIn(400);
+        });
     });
 });
