673ad0fbd7c87d2d79ff18cb1bc80f6fe280bdd8
Cargo Kult <cargokultist@gmail.com>
2012-03-03 08:03:06 +0200

    added image as a background

--- site/javascript/herbarium.js
+++ site/javascript/herbarium.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-    $.getJSON('http://www.reddit.com/r/CityPorn/.json?jsonp=?', function(json) {
-        console.log(json);
+    $.getJSON('http://www.reddit.com/r/EarthPorn/.json?jsonp=?', function(json) {
+        var $canvas = $('#wallpaper');
         $.each(json.data.children, function(i,item){
-            $('#wallpaper').delay(1500).fadeOut(500);
-	    var img = $('<img/>').attr('src', item.data.url);
-	    $('#wallpaper').empty().append(img);
-	    $('#wallpaper').fadeIn(400);
+            //$canvas.fadeIn(600);
+	    $canvas.queue(function(next) {
+        	$canvas.css('height', $(document).height());
+	        $canvas.css('width', $(document).width());
+		$canvas.css('background', 'url(' + item.data.url + ') no-repeat');
+	    	next();
+	    });
+	    $canvas.delay(1500);
+	    //$canvas.fadeOut(400);
         });
     });
 });
