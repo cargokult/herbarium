2750a74f6fb3a64aeb0df2b19e550ffd8428a45e
Cargo Kult <cargokultist@gmail.com>
2012-03-04 21:42:15 +0200

    cleanup

--- site/javascript/herbarium.js
+++ site/javascript/herbarium.js
@@ -1,16 +1,21 @@
+/*
+ * load image in background
+ * fix images from flickr
+ */
+
 $(document).ready(function() {
     $.getJSON('http://www.reddit.com/r/EarthPorn/.json?jsonp=?', function(json) {
         var $canvas = $('#wallpaper');
         $.each(json.data.children, function(i,item){
-            //$canvas.fadeIn(600);
-	    $canvas.queue(function(next) {
-        	$canvas.css('height', $(document).height());
-	        $canvas.css('width', $(document).width());
-		$canvas.css('background', 'url(' + item.data.url + ') no-repeat');
-	    	next();
-	    });
-	    $canvas.delay(1500);
-	    //$canvas.fadeOut(400);
+	        //$canvas.fadeIn(600);
+            $canvas.queue(function(next) {
+                $canvas.css('height', $(document).height());     
+                $canvas.css('width', $(document).width());
+		        $canvas.css('background', 'url(' + item.data.url + ') no-repeat');
+                next();
+	        });
+	        $canvas.delay(3000);
+	        //$canvas.fadeOut(400);
         });
     });
 });
